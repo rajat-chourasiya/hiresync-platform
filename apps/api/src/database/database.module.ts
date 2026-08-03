576cbb7afe8b68692c7518026b5df3d6f1e88f0b
Rajat <rchourasiya538@gmail.com>
2026-08-03 23:43:30 +0530

    mongodb connection established

--- apps/api/src/database/database.module.ts
+++ apps/api/src/database/database.module.ts
@@ -1,1 +1,23 @@
-// Database Module
+import { Logger, Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { tenantPlugin } from './tenant.plugin';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+        dbName: configService.get<string>('DATABASE_NAME'),
+        connectionFactory: (connection) => {
+  connection.plugin(tenantPlugin);
+  return connection;
+},
+      }),
+    }),
+  ],
+  exports: [MongooseModule],
+})
+export class DatabaseModule {}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { getConnectionToken } from '@nestjs/mongoose/dist/common/mongoose.utils';
+import { Connection } from 'mongoose';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,8 +10,14 @@ async function bootstrap() {
 
   await app.listen(5000);
 
-  console.log("Backend Running:");
-  console.log("http://localhost:5000");
+  const connection = app.get<Connection>(getConnectionToken());
+
+  if (connection.readyState === 1) {
+  console.log('🚀 Backend Running');
+  console.log('🌐 http://localhost:5000');
+  console.log('📘 Swagger: http://localhost:5000/docs');
+
+}
 }
 
 bootstrap();  
