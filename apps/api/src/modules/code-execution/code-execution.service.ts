c8080f99ac11390af0b5de6e17cc6ddd8e6b2f2f
Rajat <rchourasiya538@gmail.com>
2026-08-10 23:39:33 +0530

    docker file integrated

--- apps/api/src/modules/code-execution/code-execution.service.ts
+++ apps/api/src/modules/code-execution/code-execution.service.ts
@@ -1,1 +1,16 @@
-// code-execution.service.ts placeholder
+import { Injectable, BadRequestException } from '@nestjs/common';
+import axios from 'axios';
+
+@Injectable()
+export class CodeExecutionService {
+  async run(language: string, version: string, code: string) {
+    try {
+      const res = await axios.post(`${process.env.PISTON_RUNNER_URL}/run`, {
+        language, version, code,
+      });
+      return res.data;
+    } catch (err) {
+      throw new BadRequestException('Code execution failed');
+    }
+  }
+}
